cd32bec335403dac7e21c94d04b7813e228e9f3b
SofianeLasri <alasri250@gmail.com>
2022-01-11 19:46:05 +0100

    removed urlMode

--- core/controller/functions.php
+++ core/controller/functions.php
@@ -4,26 +4,7 @@ function loadPage(){
     global $localSettings, $urlPath;
     // S'il existe un paramètre on l'affecte à pageName
 
-    // Ici on va vérifier le mode de récupération de l'url
-    if($localSettings["urlMode"] == "parameters"){
-        // Ici on fonctionne en mode paramètres, on va donc reconstruire l'alias
-
-        $alias = array();
-        // Si le paramètre admin existe (pas besoin qu'il ai de valeur)
-        if(isset($_GET['admin'])){
-            $alias[] = "admin"; // Alors on l'ajoute à l'alias
-        }
-        // Pareil pour les pages
-        if(isset($_GET['page']) && !empty($_GET['page'])){
-            $alias[] = $_GET['page'];
-        }else{
-            // Maisi ici on va donner une valeur par défaut
-            $alias[] = "vitrine";
-        }
-    }else{
-        // Si on est en mode alias, alors on récupère directement la variable $urlPath
-        $alias = $urlPath;
-    }
+    $alias = $urlPath;
 
     if(empty($alias[0])){
         // Si le premier alias est vide, on va donc charger la page vitrine
