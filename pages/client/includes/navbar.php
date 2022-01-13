5e547cb3b7813c1ca59b8a6746422608c1d1aea1
SofianeLasri <alasri250@gmail.com>
2022-01-13 14:18:41 +0100

    navbar

--- pages/client/includes/navbar.php
+++ pages/client/includes/navbar.php
@@ -8,8 +8,16 @@
     </div>
     <div class="inferieure" style="background-image: url('<?=getWebsiteSetting("websiteUrl")?>data/images/backgrounds/bg-wood-dark.png');">
         <div class="container d-flex align-items-center h-100">
-            <div>
+            <div class="flex-grow-1">
                 <a href="<?=getWebsiteSetting("websiteUrl")?>" class="siteName">SL-Craft</a>
+            </div>
+            <div>
+                <a href="#" class="nav-link">
+                    <i class="fa">
+                        <img src="<?=getWebsiteSetting("websiteUrl")?>data/images/navbar/menu-buy.gif" alt="Join icon">
+                    </i>
+                    Nous rejoindre
+                </a>
             </div>
         </div>
     </div>
