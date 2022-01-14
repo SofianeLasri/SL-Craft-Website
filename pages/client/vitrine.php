8fd1e62b07ad7b218c5b75e152b4ebb117d280d7
SofianeLasri <alasri250@gmail.com>
2022-01-14 14:22:33 +0100

    modal-dialog

--- pages/client/vitrine.php
+++ pages/client/vitrine.php
@@ -45,20 +45,17 @@
     <!-- connectModal -->
     <div class="modal fade" id="connectModal">
         <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Rejoindre le serveur</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Rejoindre le serveur</h5>
+                </div>
+                <div class="modal-body">
+                ...
+                </div>
+                <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
             </div>
-            <div class="modal-body">
-            ...
-            </div>
-            <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
-        </div>
         </div>
     </div>
 
