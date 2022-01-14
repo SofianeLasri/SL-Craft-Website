7112e3265de774ccd7decb3ed2c1c73908288dee
SofianeLasri <alasri250@gmail.com>
2022-01-14 17:16:11 +0100

    bugfix

--- core/classes/Shop.php
+++ core/classes/Shop.php
@@ -3,17 +3,24 @@ class Shop{
     public static function getAllProducts(){
         $itemConfig = Connexion::pdo()->query("SELECT itemConfig FROM qs_shops")->fetchColumn();
         $items = yaml_parse($itemConfig);
+
+        /*
+        item:
+            ==: org.bukkit.inventory.ItemStack
+            v: 2865
+            type: WHEAT
+        */
         
         $return = array();
         foreach($items as $item){
             $itemAleadyEntered = false;
             foreach($return as $checkItem){
-                if($item == $checkItem->getId()){
+                if($item->item->type == $checkItem->getId()){
                     $itemAleadyEntered = true;
                 }
             }
             if(!$itemAleadyEntered){
-                $return[] = new Item($item);
+                $return[] = new Item($item->item->type);
             }
         }
         return $return;
