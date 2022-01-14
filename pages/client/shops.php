56c3bf21e1151495ce155346622b5c36a8a0c8d7
SofianeLasri <alasri250@gmail.com>
2022-01-14 15:43:39 +0100

    test

--- pages/client/shops.php
+++ pages/client/shops.php
@@ -35,6 +35,16 @@
                             <option>3</option>
                             <option>4</option>
                             <option>5</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
                         </select>
                     </div>
                     <div class="mc-button normal" onClick="javascript:this.form.submit();">
