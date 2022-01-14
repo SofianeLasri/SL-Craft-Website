1ef90667914f72bbf3271145a2baa9722f3a6a5a
SofianeLasri <alasri250@gmail.com>
2022-01-14 17:43:49 +0100

    test card

--- pages/client/shops.php
+++ pages/client/shops.php
@@ -53,6 +53,22 @@
             </div>
             <div class="col-md-10">
                 <h3>Liste des magasins</h3>
+
+                <div class="row">
+                    <div class="col-sm">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#">
+                                    <div class="mc-button normal">
+                                        <div class="title">Se téléporter</div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
