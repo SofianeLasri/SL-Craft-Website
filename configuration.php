e8083e3724c9b2a25907b21f07edb8625dde623d
SofianeLasri <alasri250@gmail.com>
2022-04-09 19:18:53 +0200

    passage en base de prod

--- configuration.php
+++ configuration.php
@@ -1,6 +1,6 @@
 <?php
 // Informations de connexion à la base de données
 $config['db']['hostName'] = 'node.sl-projects.com';
-$config['db']['databaseName'] = 'srvMinecraftDev';
+$config['db']['databaseName'] = 'srvMinecraft';
 $config['db']['userName'] = 'srvMinecraft';
 $config['db']['userPassword'] = 'YIn2hTln5iMQap1W';
